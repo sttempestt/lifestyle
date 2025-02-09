2bbbe183d43c4c3f199cb1606a7f2be3b7bfc7b9
sttempestt <random.name611@gmail.com>
2025-02-10 00:14:00 +0200

    staged models

--- expenses/models.py
+++ expenses/models.py
@@ -10,18 +10,18 @@ class Category(models.Model):
 class Transactions(models.Model):
     R = 'R'
     S = 'S'
-    types = {
+    types = [
         (R, 'received'),
         (S, 'spent'),
-    }
+    ]
     E = 'E'
     C = 'C'
     P = 'P'
-    statuses = {
+    statuses = [
         (E, 'expected'),
         (C, 'completed'),
         (P, 'pending'),
-    }
+    ]
     description = models.CharField(max_length=50, default="", blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     sum = models.DecimalField(max_digits=10, decimal_places=2)
@@ -31,12 +31,12 @@ class Transactions(models.Model):
     date_expected = models.DateTimeField(null=True)
     status = models.CharField(choices=statuses, max_length=1, default=C, blank=True)
     def __str__(self):
+        formatted_date = self.date_completed.strftime("%Y-%m-%d %H:%M")  
+        account_display = self.get_account_display()
         if self.status == 'C':
-            return f"{self.date_completed}:{self.sum} {self.account}, {self.description}"
+            return f"{formatted_date}: {self.sum} {account_display}, {self.description}"
         else:
-            return f"{self.status} {self.date_completed}:{self.sum} {self.account}, {self.description}"
-    
-    
-   
-""">>> Transactions.objects.all()
-<QuerySet [<Transactions: 2025-02-04 23:24:57.691178+00:00:2.00 R, >]>"""
+            return f"{self.status} {formatted_date}: {self.sum} {account_display}, {self.description}"
+        
+
+#bob
