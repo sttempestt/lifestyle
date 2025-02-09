4c7f9fb4a84feb13d7ff6cf9dcc630137adaaa8a
azay <shubin.yan@tftc.dev>
2025-02-10 00:09:05 +0200

    feature: ended regitration and login

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import CustomUser
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label="Username or Email")
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "password1", "password2"]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
-from .forms import CustomAuthenticationForm
+from django.contrib.auth import login
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
 
 # Create your views here.
 
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = "users/login.html"
+
+def registration(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "users/registration.html", {"form": form})
